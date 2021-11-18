7932518dd5697d7cde944369efb9c4770c1025e1
Torsten Martinsen <torsten@bullestock.net>
2021-11-18 20:38:17 +0100

    Avoid stack overflow in camera task.

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -30,5 +30,5 @@ void app_main()
     ESP_ERROR_CHECK(example_connect());
     ESP_LOGI(TAG, "Connected to WiFi. Instance #%d", CONFIG_HAL32CAM_INSTANCE);
 
-    xTaskCreate(&camera_task, "camera_task", 8192, nullptr, 5, nullptr);
+    xTaskCreate(&camera_task, "camera_task", 32768, nullptr, 5, nullptr);
 }
