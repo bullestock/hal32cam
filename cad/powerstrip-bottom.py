274104f882a2c8ec24621f2c4809c1ae58649798
Torsten Martinsen <torsten@bullestock.net>
2026-07-11 10:21:38 +0200

    Add support for flange.

--- cad/powerstrip-bottom.py
+++ cad/powerstrip-bottom.py
@@ -1,4 +1,4 @@
-# A box with a row of USB-C female connectors - upper part
+# A box with a row of USB-C female connectors - lower part
 
 # %%
 from build123d import *
@@ -6,6 +6,12 @@ from ocp_vscode import *
 from epilogue import *
 
 from powerstrip_defs import *
+
+ADD_FLANGES = True
+
+flange_w = 10
+flange_l = 10
+hole_d = 4.5
 
 width = N * p_cc
 
@@ -19,5 +25,13 @@ with BuildPart() as p:
     with BuildSketch(p.faces().sort_by(Axis.Z).last):
         RectangleRounded(width-2*inset, length-2*inset, 2)
     extrude(amount=-(bot_h-inset), mode=Mode.SUBTRACT)
+    if ADD_FLANGES:
+        with BuildSketch():
+            RectangleRounded(width + 2*flange_l, flange_w, 2)
+        extrude(amount=inset)
+        with BuildSketch():
+            with GridLocations(width + flange_l, 1, 2, 1):
+                Circle(hole_d/2)
+        extrude(amount=inset, mode=Mode.SUBTRACT)
 
 epilogue(p)
